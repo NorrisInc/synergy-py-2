2278356f626cfb1062d567a9f4c6bd956191f130
Михаил Шатов <4me0ns@gmail.com>
2026-01-28 07:13:28 +0300

    fixes

--- lesson3_2.py
+++ lesson3_2.py
@@ -8,21 +8,20 @@ defaultQuestions = [
     ["Homo heidelbergensis", ""],
     ["Homo sapiens", ""],]
 
-questions = [
-]
+questions = []
 
 def main():
     print("Урок 3.2 - Тест по биологии")
     questions = defaultQuestions
 
-    for i in enumerate(questions):
-        questions[i][1] = input(f"Стадия № {i + 1}")
+    for i, (index, _) in enumerate(questions):
+        questions[i][1] = input(f"Стадия № {(i + 1)}: ")
 
-    grade = -1
+    grade = 0
     for i, (answer, question) in enumerate(questions):
         if answer == question:
             grade = grade + 1
-    if grade < 0:
+    if grade < 2:
         grade = 2
     print(f'\nОценка: {grade}')
 
